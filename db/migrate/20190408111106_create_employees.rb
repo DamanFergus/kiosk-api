30b2635c6b3c8d29345f1f3796933b8bf3b124d7
Daman <daman.fergus@justice.gov.uk>
2019-04-08 12:18:54 +0100

    inital commit

---
+++ db/migrate/20190408111106_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.integer :empid
+      t.string :name
+      t.string :email
+      t.integer :mobnum
+      t.decimal :balance
+      t.integer :pin
+
+      t.timestamps
+    end
+  end
+end
