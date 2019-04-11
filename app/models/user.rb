76e8b4b08b98a95229cb07200c59b1fe700b932d
Daman <daman.fergus@justice.gov.uk>
2019-04-11 15:08:27 +0100

    implemented user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # Model associations
-  # has_one :cards, foreign_key: :id
+  has_many :cards, foreign_key: :created_by
   # Validations
   validates_presence_of :empid, :mobile, :name, :email, :password_digest
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 # Test suite for User model
 RSpec.describe User, type: :model do
   # Association test
-  # ensure User model has a 1:m relationship with the Todo model
-  #it { should have_one(:card) }
+  it { should have_many(:cards) }
   # Validation tests
-  # ensure name, email and password_digest are present before save
   it { should validate_presence_of(:empid) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:mobile) }
